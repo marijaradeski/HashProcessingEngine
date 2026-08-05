f875e27b1df87b9556eab0ade1e972a8f7f24ce9
user <user@DESKTOP-RNM89PG>
2026-08-05 23:44:56 +0200

    default value 40000

--- src/HashProcessingEngine.Application/DTOs/GenerateHashesRequest.cs
+++ src/HashProcessingEngine.Application/DTOs/GenerateHashesRequest.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace HashProcessingEngine.Application.DTOs;
 public class GenerateHashesRequest
 {
     [Range(1, 60000, ErrorMessage = "Count must be between 1 and 60000.")]
+    [DefaultValue(40000)]
     public int? Count { get; set; } = 40000;
 }
